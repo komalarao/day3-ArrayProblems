d58fe2e3cbb792cca05033e028aa6c97d49a4262
komalarao <manoharvakamullu1999@gmail.com>
2023-01-08 07:10:54 -0800

    Refactor: array program to sort array in ascending order

--- src/com/bridgelabz/array_problems/AscendingOrderArray.java
+++ src/com/bridgelabz/array_problems/AscendingOrderArray.java
@@ -1,24 +1,29 @@
 package com.bridgelabz.array_problems;
 import java.util.Scanner;
-public class DuplicateElementArray {
+public class AscendingOrderArray {
 
 	public static void main(String[] args) {
       Scanner elements =new Scanner(System.in);
       System.out.println(" enter the size of array");
       int size = elements.nextInt();
       int Numbers_Array[] = new int[size];
-        
+      int temporary;
     	  System.out.println("enter "+size+" elements of array");
       for(int i=0;i<size;i++) {
     	  Numbers_Array[i] = elements.nextInt();
     	  
       }for (int i=0;i<Numbers_Array.length;i++) {
     	 for(int j=i+1;j<Numbers_Array.length;j++) {
-    		 if(Numbers_Array[i]==Numbers_Array[j]) {
-    			 System.out.println(Numbers_Array[i]);
+    		 if(Numbers_Array[i]>Numbers_Array[j]) {
+    			 temporary = Numbers_Array[i];
+    			 Numbers_Array[i]=Numbers_Array[j];
+    			 Numbers_Array[j]=temporary;
     		 }
     	 }
     	  
+      }System.out.println("ascending order of array is:");
+      for(int i=0;i<Numbers_Array.length;i++) {
+    	  System.out.println(Numbers_Array[i]);
       }
       
 	}
